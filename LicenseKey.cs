e2361ff0ac9fbae7f3649a68220d6e69f891114d
Admin <Admin@DEVOPS>
2025-03-19 01:27:37 +0300

    auto update

--- LicenseKey.cs
+++ LicenseKey.cs
@@ -4,8 +4,14 @@
     {
         public int Id { get; set; }
         public string Key { get; set; } = string.Empty;
-        public DateTime ExpirationDate { get; set; }
         public bool IsUnlimited { get; set; } = false;
-    }
+    
+        public DateTime ExpirationDate
+        {
+            get => _expirationDate;
+            set => _expirationDate = DateTime.SpecifyKind(value, DateTimeKind.Utc);
+        }
 
+        private DateTime _expirationDate;
+    }
 }
