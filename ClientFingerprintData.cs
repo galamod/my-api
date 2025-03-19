c0801451c7f2fcdfeb23a1f370d22e829836ad7e
Admin <Admin@DEVOPS>
2025-03-20 00:48:18 +0300

    обнова

--- ClientFingerprintData.cs
+++ ClientFingerprintData.cs
@@ -2,7 +2,7 @@
 {
     public class ClientFingerprintData
     {
-        public string IPJson { get; set; } = string.Empty;
+        public string UserAgent { get; set; } = string.Empty;
         public string ScreenResolution { get; set; } = string.Empty;
         public string Timezone { get; set; } = string.Empty;
     }
--- LicenseKey.cs
+++ LicenseKey.cs
@@ -5,23 +5,13 @@
         public int Id { get; set; }
         public string Key { get; set; } = string.Empty;
         public bool IsUnlimited { get; set; } = false;
-        public string? UserIdentifier { get; set; }
-
-        private DateTime? _trialStart;
-        public DateTime? TrialStart
-        {
-            get => _trialStart;
-            set => _trialStart = value.HasValue
-                ? DateTime.SpecifyKind(value.Value, DateTimeKind.Utc)
-                : null;
-        }
-
-        private DateTime _expirationDate;
+    
         public DateTime ExpirationDate
         {
             get => _expirationDate;
             set => _expirationDate = DateTime.SpecifyKind(value, DateTimeKind.Utc);
         }
-    }
 
+        private DateTime _expirationDate;
+    }
 }
